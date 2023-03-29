68bca01c799ad1f313f745aff5b6732c1782a630
Shafi <shadikulshafi@yahoo.com>
2023-03-29 14:06:44 +0600

    controller and red eye addded

--- lib/pages/log_in.dart
+++ lib/pages/log_in.dart
@@ -2,19 +2,30 @@ import 'package:amar_institute/widgets/input_field.dart';
 import 'package:flutter/material.dart';
 
 class LogInPage extends StatefulWidget {
-  const LogInPage({Key? key}) : super(key: key);
+
+  const LogInPage({Key? key,}) : super(key: key);
 
   @override
   State<LogInPage> createState() => _LogInPageState();
 }
-
 class _LogInPageState extends State<LogInPage> {
+
+  void ButtonPress(){
+    print(userController.text);
+    print(passwordController.text);
+    print("change");
+  }
+
+  bool _show = true;
+  final userController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         alignment: Alignment.center,
-        padding: EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children:  [
@@ -25,11 +36,30 @@ class _LogInPageState extends State<LogInPage> {
            const SizedBox(
              height: 50,
            ),
-            InputField(labelText: "User Name", hintText: "amarinstitute@exapmle.com",),
-        const SizedBox(
-          height: 15,
-        ),
-            InputField(labelText: "Password",)
+            InputField(
+              labelText: "User Name",
+              hintText: "amarinstitute@exapmle.com",
+              obcecureText: false,
+              controller: userController,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            InputField(
+              labelText: "Password",
+              obcecureText: _show,
+              controller: passwordController,
+              passwordIcon: IconButton(
+                icon: Icon(Icons.remove_red_eye),
+                onPressed: (){
+                  setState(() {
+                    _show = !_show;
+                  });
+                },),),
+            const SizedBox(
+              height: 15,
+            ),
+            ElevatedButton(onPressed: (){ButtonPress();},  child: Text("log in"))
           ],
         ),
       ),
--- lib/widgets/input_field.dart
+++ lib/widgets/input_field.dart
@@ -3,22 +3,33 @@ import 'package:flutter/material.dart';
 class InputField extends StatelessWidget {
 
   String? hintText, labelText;
+  final passwordIcon, controller;
+  bool obcecureText = true;
 
-
-  InputField({Key? key, this.labelText, this.hintText} ) : super(key: key);
+  InputField({Key? key,
+    this.labelText,
+    this.hintText,
+    this.passwordIcon,
+    required this.controller,
+    required this.obcecureText
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(top: 15),
+      //margin: const EdgeInsets.only(top: 15),
       child: TextField(
         decoration: InputDecoration(
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(15)
           ),
           hintText:hintText,
-          labelText: labelText
-        ),
+          labelText: labelText,
+          suffixIcon: passwordIcon,
+      ),
+        obscureText: obcecureText,
+        obscuringCharacter: "*",
+        controller: controller,
       )
     );
 
